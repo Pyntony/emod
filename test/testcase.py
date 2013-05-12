df8aa4ffe31852f9848dce80c87f7803a0cfcd81
Axujen <axujen@gmail.com>
2013-05-12 21:13:46 +0000

    Test save_rules() for correct output.
    Added a testcase for save_rules() to make sure its saving files as it is
    supposed to.
    
    Only testing file structures, directories are going to be a little
    trickier to compare.

--- test/testcase.py
+++ test/testcase.py
@@ -3,7 +3,7 @@
 
 import unittest
 import sys
-import os.path
+import os
 
 # Assuming that the test directory is inside the emod directory, this will allow
 # emod to be imported if the script is run either from the emod directory
@@ -84,6 +84,120 @@ class read_rules(unittest.TestCase):
         """read_rules must return valid output for directory."""
         infile = 'package.expressions.dir'
         self.assertEqual(emod.read_rules(infile), self.out)
+
+class save_rules(unittest.TestCase):
+    rules = ['!!<sys-apps/portage-2.1.4_rc1\n',
+            # rules to be saved by the test methods
+           '!=net-fs/samba-2*\n',
+           '!app-text/dos2unix\n',
+           '*/*\n',
+           '*/*::gentoo\n',
+           '*/zlib\n',
+           '<=media-libs/libgd-1.6\n',
+           '<media-libs/libgd-1.6\n',
+           '=*/*-*9999*\n',
+           '=*/*-*_beta*\n',
+           '=dev-libs/glib-2*\n',
+           'dev-libs/glib:2/2.30=\n',
+           '=media-libs/libgd-1.6\n',
+           '=x11-libs/qt-3.3*:3\n',
+           'dev-libs/icu:0/49\n',
+           'dev-libs/icu:0/49=\n',
+           'dev-libs/icu:0=\n',
+           '>=media-libs/libgd-1.6\n',
+           '>=x11-libs/qt-3.3.8:3\n',
+           '>media-libs/libgd-1.6\n',
+           'dev-lang/perl:*\n',
+           'dev-lang/perl:0/5.12\n',
+           'dev-lang/perl:0/5.12=\n',
+           'dev-libs/glib:*\n',
+           'dev-libs/glib:2/2.30\n',
+           'dev-lang/perl:=\n',
+           'dev-lang/perl:0=\n',
+           'dev-libs/icu:*\n',
+           'dev-libs/icu:=\n',
+           'kde-base/kdelibs::kde-testing\n',
+           'net-*/*\n',
+           'net-im/empathy::gnome\n',
+           'dev-libs/glib:2=\n',
+           'dev-libs/glib:=\n',
+           'dev-libs/icu:0/0\n',
+           'dev-libs/icu:0/0=\n',
+           'net-misc/dhcp-3.0_p2\n',
+           'sys-apps/*\n',
+           'sys-apps/sed\n',
+           'sys-apps/sed-4.0.5\n',
+           'net-misc/dhcp\n',
+           'sys-apps/sed::gentoo\n',
+           'sys-libs/zlib\n',
+           'sys-libs/zlib-1.1.4-r1\n',
+           'x11-libs/qt:3\n',
+           '~net-libs/libnet-1.0.2a\n',
+           '~x11-libs/qt-3.3.8:3\n']
+
+    sorted_rules = ['!!<sys-apps/portage-2.1.4_rc1\n',
+            # save_rules should save rules sorted.
+           '!=net-fs/samba-2*\n',
+           '!app-text/dos2unix\n',
+           '*/*\n',
+           '*/*::gentoo\n',
+           '*/zlib\n',
+           '<=media-libs/libgd-1.6\n',
+           '<media-libs/libgd-1.6\n',
+           '=*/*-*9999*\n',
+           '=*/*-*_beta*\n',
+           '=dev-libs/glib-2*\n',
+           '=media-libs/libgd-1.6\n',
+           '=x11-libs/qt-3.3*:3\n',
+           '>=media-libs/libgd-1.6\n',
+           '>=x11-libs/qt-3.3.8:3\n',
+           '>media-libs/libgd-1.6\n',
+           'dev-lang/perl:*\n',
+           'dev-lang/perl:0/5.12\n',
+           'dev-lang/perl:0/5.12=\n',
+           'dev-lang/perl:0=\n',
+           'dev-lang/perl:=\n',
+           'dev-libs/glib:*\n',
+           'dev-libs/glib:2/2.30\n',
+           'dev-libs/glib:2/2.30=\n',
+           'dev-libs/glib:2=\n',
+           'dev-libs/glib:=\n',
+           'dev-libs/icu:*\n',
+           'dev-libs/icu:0/0\n',
+           'dev-libs/icu:0/0=\n',
+           'dev-libs/icu:0/49\n',
+           'dev-libs/icu:0/49=\n',
+           'dev-libs/icu:0=\n',
+           'dev-libs/icu:=\n',
+           'kde-base/kdelibs::kde-testing\n',
+           'net-*/*\n',
+           'net-im/empathy::gnome\n',
+           'net-misc/dhcp\n',
+           'net-misc/dhcp-3.0_p2\n',
+           'sys-apps/*\n',
+           'sys-apps/sed\n',
+           'sys-apps/sed-4.0.5\n',
+           'sys-apps/sed::gentoo\n',
+           'sys-libs/zlib\n',
+           'sys-libs/zlib-1.1.4-r1\n',
+           'x11-libs/qt:3\n',
+           '~net-libs/libnet-1.0.2a\n',
+           '~x11-libs/qt-3.3.8:3\n']
+
+    maxDiff =  None # Disable diff limit
+
+    def test_save_rules_file(self):
+        """save_rules should save rules in a file correctly."""
+        outfile = 'package.expressions.file.tmp'
+        # Create the file so save_rules() can detect the structure type.
+        open(outfile, 'a').close()
+        emod.save_rules(outfile, self.rules)
+
+        try:
+            self.assertEqual(open(outfile).readlines(), self.sorted_rules)
+        finally:
+            # Clean up
+            os.remove(outfile)
 
 if __name__ == '__main__':
     unittest.main()
