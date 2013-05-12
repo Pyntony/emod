bbc4e0b7f93df210d1e5cc80b319c5b2878a34fb
Axujen <axujen@gmail.com>
2013-05-12 14:08:15 +0000

    Add if main block.
    Using if main block for script execution to prevent it from going off
    when importing it.

--- emod.py
+++ emod.py
@@ -174,106 +174,109 @@ parser.add_argument('--style', type=str, default="default", choices=PKG_STYLES,
 parser.add_argument('--pkg-file', type=str, dest='pkg_file', metavar='file',
     default='/etc/portage/package',
     help='Specify a package file/directory (for testing/debugging)')
-args = parser.parse_args()
 
-category  = re.sub('[<=>]', '', args.atom.split('/')[0])
-pkg_style = args.style.lower()
+if __name__ == '__main__':
 
-# Detect the package type if the full path is given ex (/etc/portage/package.use)
-extension = os.path.splitext(args.pkg_file)[-1][1:] # grab the extension
-if extension in PKG_TYPES:
-    pkg_type = extension
-    PKG_FILE = args.pkg_file
-else:
-    pkg_type  = args.type.lower()
-    PKG_FILE = '.'.join((args.pkg_file, pkg_type))
+    args = parser.parse_args()
 
-# First determine if PKG_FILE exists and whether it is a file or a
-# directory.
-cur_pkg_style = 'default'
-if os.path.exists(PKG_FILE):
-    if os.path.isdir(PKG_FILE):
-        cur_pkg_style = 'directory'
+    category  = re.sub('[<=>]', '', args.atom.split('/')[0])
+    pkg_style = args.style.lower()
+
+    # Detect the package type if the full path is given ex (/etc/portage/package.use)
+    extension = os.path.splitext(args.pkg_file)[-1][1:] # grab the extension
+    if extension in PKG_TYPES:
+        pkg_type = extension
+        PKG_FILE = args.pkg_file
     else:
-        cur_pkg_style = 'file'
+        pkg_type  = args.type.lower()
+        PKG_FILE = '.'.join((args.pkg_file, pkg_type))
 
-# Determine if it needs a conversion.
-if cur_pkg_style == 'directory' and pkg_style == 'file':
-    directory_to_file(PKG_FILE)
-elif cur_pkg_style == 'file' and pkg_style == 'directory':
-    file_to_directory(PKG_FILE)
-elif pkg_style == 'default':
-    if cur_pkg_style == 'file':
-        pkg_style = 'file'
-    else:
-        pkg_style = 'directory'
-
-# Switch to the right file if PKG_FILE is a directory
-if pkg_style == 'directory':
-    if not os.path.exists(PKG_FILE):
-        try:
-            os.mkdir(PKG_FILE, mode=0o755)
-        except OSError:
-            sys.exit("Unable to create directory %s, are you root?" % PKG_FILE)
-        PKG_FILE = os.path.join(PKG_FILE, category)
-
-rules = read_rules(PKG_FILE)
-
-if pkg_type in ['mask', 'unmask']:
-    # Handle particular case of package.(un)mask that do not accept flags.
-    if args.prune:
-        try:
-            rules.remove(args.atom)
-        except ValueError:
-            sys.exit("%s is not %sed." % (args.atom, pkg_type))
-    else:
-        if args.atom in rules:
-            sys.exit("%s is already %sed." % (args.atom, pkg_type))
+    # First determine if PKG_FILE exists and whether it is a file or a
+    # directory.
+    cur_pkg_style = 'default'
+    if os.path.exists(PKG_FILE):
+        if os.path.isdir(PKG_FILE):
+            cur_pkg_style = 'directory'
         else:
-            rules.append(args.atom)
-            rules.sort()
-else:
-    # Retrieve the current flags if the rule exist.
-    flags = []
-    for rule in rules:
-        if rule.startswith(args.atom + " "):
-            sys.stdout.write("Old rule: " + rule)
-            flags = rule.split()[1:]
-            rules.remove(rule) # We remove the rule to update it.
-            break
-    if not flags:
-        print("No argument currently defined for %s." % args.atom)
+            cur_pkg_style = 'file'
 
-    # 1.Prune flags
-    if args.prune:
-        flags = []
+    # Determine if it needs a conversion.
+    if cur_pkg_style == 'directory' and pkg_style == 'file':
+        directory_to_file(PKG_FILE)
+    elif cur_pkg_style == 'file' and pkg_style == 'directory':
+        file_to_directory(PKG_FILE)
+    elif pkg_style == 'default':
+        if cur_pkg_style == 'file':
+            pkg_style = 'file'
+        else:
+            pkg_style = 'directory'
 
-    # 2.Disable flags
-    if args.disable:
-        for flag in args.disable:
+    # Switch to the right file if PKG_FILE is a directory
+    if pkg_style == 'directory':
+        if not os.path.exists(PKG_FILE):
             try:
-                flags.remove(flag)
+                os.mkdir(PKG_FILE, mode=0o755)
+            except OSError:
+                sys.exit("Unable to create directory %s, are you root?" % PKG_FILE)
+            PKG_FILE = os.path.join(PKG_FILE, category)
+
+    rules = read_rules(PKG_FILE)
+
+    if pkg_type in ['mask', 'unmask']:
+        # Handle particular case of package.(un)mask that do not accept flags.
+        if args.prune:
+            try:
+                rules.remove(args.atom)
             except ValueError:
-                print("warning: %s is not enabled!" % flag)
-
-    # 3.Enable flags
-    if args.enable:
-        for flag in args.enable:
-            if flag in flags:
-                print("warning: %s is already enabled!" % flag)
+                sys.exit("%s is not %sed." % (args.atom, pkg_type))
+        else:
+            if args.atom in rules:
+                sys.exit("%s is already %sed." % (args.atom, pkg_type))
             else:
-                flags.append(flag)
+                rules.append(args.atom)
+                rules.sort()
+    else:
+        # Retrieve the current flags if the rule exist.
+        flags = []
+        for rule in rules:
+            if rule.startswith(args.atom + " "):
+                sys.stdout.write("Old rule: " + rule)
+                flags = rule.split()[1:]
+                rules.remove(rule) # We remove the rule to update it.
+                break
+        if not flags:
+            print("No argument currently defined for %s." % args.atom)
 
-    # Update the rule
-    if flags:
-        flags.sort()
-        rule = args.atom + ' ' + ' '.join(flags) + '\n'
-        sys.stdout.write("New rule: " + rule)
-        rules.append(rule)
-        rules.sort()
+        # 1.Prune flags
+        if args.prune:
+            flags = []
 
-# Save changes
-try:
-    save_rules(PKG_FILE, rules)
-except IOError:
-    sys.exit("Unable to write in %s, are you root?" % PKG_FILE)
+        # 2.Disable flags
+        if args.disable:
+            for flag in args.disable:
+                try:
+                    flags.remove(flag)
+                except ValueError:
+                    print("warning: %s is not enabled!" % flag)
+
+        # 3.Enable flags
+        if args.enable:
+            for flag in args.enable:
+                if flag in flags:
+                    print("warning: %s is already enabled!" % flag)
+                else:
+                    flags.append(flag)
+
+        # Update the rule
+        if flags:
+            flags.sort()
+            rule = args.atom + ' ' + ' '.join(flags) + '\n'
+            sys.stdout.write("New rule: " + rule)
+            rules.append(rule)
+            rules.sort()
+
+    # Save changes
+    try:
+        save_rules(PKG_FILE, rules)
+    except IOError:
+        sys.exit("Unable to write in %s, are you root?" % PKG_FILE)
